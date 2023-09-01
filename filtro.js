e4bc8de3135d0e696d78ad5e0a03f33e9dd7e855
diegoanh <diego.a.herrera.garcia@gmail.com>
2023-09-01 10:53:41 -0600

    first

--- filtro.js
+++ filtro.js
@@ -8,8 +8,8 @@ const productos = [
   {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
 ]
 
-const li = document.getElementsByName("lista-de-productos")
-const $i = document.querySelector('.input');
+const li = document.getElementById("lista-de-productos"); // Cambiar a I
+const $i = document.querySelector('input');
 
 for (let i = 0; i < productos.length; i++) {
   var d = document.createElement("div")
